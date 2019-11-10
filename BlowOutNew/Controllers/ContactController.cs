73242f7033ac0b48ffa97defffb3410595546bf7
Ashlyn Lewis <act56@byu.edu>
2019-11-10 15:08:21 -0700

    Add project files.

---
+++ BlowOutNew/Controllers/ContactController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOutNew.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Email (string name, string email)
+        {
+           ViewBag.Name = name;
+            ViewBag.Email = email;
+            
+            return View("Email");
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+
+        //Possible code for sending smtp emails
+        //[HttpGet]
+        //public ActionResult ContactUs()
+        //{
+
+        //    return View();
+        //}
+
+        //public void SendEmail(string toAddress, string fromAddress,
+        //              string subject, string message)
+        //{
+        //    try
+        //    {
+        //        using (var mail = new MailMessage())
+        //        {
+        //            const string email = "ashlynlewis1@gmail.com";
+        //            const string password = "trashtan143";
+
+        //            var loginInfo = new System.Net.NetworkCredential(email, password);
+
+
+        //            mail.From = new MailAddress(fromAddress);
+        //            mail.To.Add(new MailAddress(toAddress));
+        //            mail.Subject = subject;
+        //            mail.Body = message;
+        //            mail.IsBodyHtml = true;
+
+        //            try
+        //            {
+        //                using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+        //                {
+        //                    smtpClient.EnableSsl = true;
+        //                    smtpClient.UseDefaultCredentials = false;
+        //                    smtpClient.Credentials = loginInfo;
+        //                    smtpClient.Send(mail);
+        //                }
+
+        //            }
+
+        //            finally
+        //            {
+        //                //dispose the client
+        //                mail.Dispose();
+        //            }
+
+        //        }
+        //    }
+        //    catch (SmtpFailedRecipientsException ex)
+        //    {
+        //        foreach (SmtpFailedRecipientException t in ex.InnerExceptions)
+        //        {
+        //            var status = t.StatusCode;
+        //            if (status == SmtpStatusCode.MailboxBusy ||
+        //                status == SmtpStatusCode.MailboxUnavailable)
+        //            {
+        //                Response.Write("Delivery failed - retrying in 5 seconds.");
+        //                System.Threading.Thread.Sleep(5000);
+        //                //resend
+        //                //smtpClient.Send(message);
+        //            }
+        //            else
+        //            {
+        //                Response.Write("Failed to deliver message to {0}"
+        //                                  /*t.FailedRecipient*/);
+        //            }
+        //        }
+        //    }
+        //    catch (SmtpException Se)
+        //    {
+        //        // handle exception here
+        //        Response.Write(Se.ToString());
+        //    }
+
+        //    catch (Exception ex)
+        //    {
+        //        Response.Write(ex.ToString());
+        //    }
+
+        //}
+
+        //[HttpPost]
+        //public ActionResult Contact(Contact contact)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+
+        //        //prepare email
+        //        var toAddress = "ashlynlewis1@gmail.com";
+        //        var fromAddress = contact.EmailAddress.ToString();
+        //        var subject = "Test inquiry from " + contact.Name;
+        //        var message = new StringBuilder();
+        //        message.Append("Name: " + contact.Name + "\n");
+        //        message.Append("Email: " + contact.EmailAddress + "\n");
+        //        message.Append("phone: " + contact.phone + "\n\n");
+        //        message.Append(contact.message);
+
+        //        //start email Thread
+        //        var tEmail = new Thread(() =>
+        //       SendEmail(toAddress, fromAddress, subject, message));
+        //        tEmail.Start();
+        //    }
+        //    return View();
+        //}
+    }
+}
---
+++ BlowOutNew/Controllers/RentalsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOutNew.Controllers
+{
+    public class RentalsController : Controller
+    {
+        // GET: Rentals
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string instrumentName, string instrumentDesc)
+        {
+            ViewBag.Name = instrumentName;
+            ViewBag.Description = instrumentDesc;
+            ViewBag.ContractFull = ("Upon completion of the 18 month agreement, the customer owns the rented instrument.  If, however, the instrument is returned" +
+                " at any time before three (3) months, there will be a $200 restocking fee.  If the instrument is returned after three (3) months they forfeit any " +
+                "equity in the instrument.");
+
+            return View("Details");
+        }
+    }
+}
