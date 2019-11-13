bf66bf7d8890bf27a41b50ebf2fe135509d35be3
Ashlyn Lewis <act56@byu.edu>
2019-11-12 21:17:31 -0700

    made it work

--- BlowOutNew/App_Start/RouteConfig.cs
+++ BlowOutNew/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace BlowOutNew
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "Email",
+               url: "{controller}/{action}/{firstName}/{lastName}/{emailAddress}",
+               defaults: new { controller = "Contact", action = "Email", firstName = UrlParameter.Optional, lastName = UrlParameter.Optional, emailAddress = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
--- BlowOutNew/Controllers/ContactController.cs
+++ BlowOutNew/Controllers/ContactController.cs
@@ -16,16 +16,18 @@ namespace BlowOutNew.Controllers
         // GET: Contact
         public ActionResult Index()
         {
+            ViewBag.Support = "Please call Support at<strong> < u > 801 - 555 - 1212.</ u ></ strong > Thank you!";
 
-            return View();
+            return View("Contact");
         }
 
-        public ActionResult Email (string name, string email)
+        
+        public ActionResult Email(string firstName, string lastName, string emailAddress)
         {
-            ViewBag.Name = name;
-            ViewBag.Email = email;
-            
-            return View("Email");
+            ViewBag.Name = firstName + " " + lastName;
+            ViewBag.Email = emailAddress;
+
+            return View();
         }
 
         public ActionResult AboutUs()
@@ -43,100 +45,114 @@ namespace BlowOutNew.Controllers
             return View();
         }
 
-        public void SendEmail(string toAddress, string fromAddress,
-                      string subject, string message)
-        {
-            try
-            {
-                using (var mail = new MailMessage())
-                {
-                    const string email = "ashlynlewis1@gmail.com";
-                    const string password = "trashtan143";
-
-                    var loginInfo = new System.Net.NetworkCredential(email, password);
-
-
-                    mail.From = new MailAddress(fromAddress);
-                    mail.To.Add(new MailAddress(toAddress));
-                    mail.Subject = subject;
-                    mail.Body = message;
-                    mail.IsBodyHtml = true;
-
-                    try
-                    {
-                        using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
-                        {
-                            smtpClient.EnableSsl = true;
-                            smtpClient.UseDefaultCredentials = false;
-                            smtpClient.Credentials = loginInfo;
-                            smtpClient.Send(mail);
-                        }
-
-                    }
-
-                    finally
-                    {
-                        //dispose the client
-                        mail.Dispose();
-                    }
-
-                }
-            }
-            catch (SmtpFailedRecipientsException ex)
-            {
-                foreach (SmtpFailedRecipientException t in ex.InnerExceptions)
-                {
-                    var status = t.StatusCode;
-                    if (status == SmtpStatusCode.MailboxBusy ||
-                        status == SmtpStatusCode.MailboxUnavailable)
-                    {
-                        Response.Write("Delivery failed - retrying in 5 seconds.");
-                        System.Threading.Thread.Sleep(5000);
-                        //resend
-                        //smtpClient.Send(message);
-                    }
-                    else
-                    {
-                        Response.Write("Failed to deliver message to {0}"
-                                          /*t.FailedRecipient*/);
-                    }
-                }
-            }
-            catch (SmtpException Se)
-            {
-                // handle exception here
-                Response.Write(Se.ToString());
-            }
-
-            catch (Exception ex)
-            {
-                Response.Write(ex.ToString());
-            }
-
-        }
-
         [HttpPost]
         public ActionResult Contact(ContactRequest contact)
         {
-            if (ModelState.IsValid)
-            {
-                //prepare email
-                var toAddress = "ashlynlewis1@gmail.com";
-                var fromAddress = contact.emailAddress.ToString();
-                var subject = "Test inquiry from " + contact.subject;
-                var message = new StringBuilder();
-                message.Append("Name: " + contact.firstName + contact.lastName + "\n");
-                message.Append("Email: " + contact.emailAddress + "\n");
-                message.Append("phone: " + contact.phoneNumber + "\n");
-                message.Append("Subject: " + contact.subject + "\n\n");
-                message.Append(contact.Message);
+            ViewBag.Name = contact.firstName + " " + contact.lastName;
+            ViewBag.Email = contact.emailAddress;
 
-                //start email Thread
-                var tEmail = new Thread(() =>
-               SendEmail(toAddress, fromAddress, subject, contact.Message));
-                tEmail.Start();
-            }
-            return View("Email", contact);
+            return View("Email",contact);
         }
+
+        //public void SendEmail(string toAddress, string fromAddress,
+        //              string subject, string message)
+        //{
+        //    try
+        //    {
+        //        using (var mail = new MailMessage())
+        //        {
+        //            const string email = "is403test@gmail.com";
+        //            const string password = "trashtan143";
+
+        //            var loginInfo = new System.Net.NetworkCredential(email, password);
+
+
+        //            mail.From = new MailAddress(fromAddress);
+        //            mail.To.Add(new MailAddress(toAddress));
+        //            mail.Subject = subject;
+        //            mail.Body = message;
+        //            mail.IsBodyHtml = true;
+
+        //            try
+        //            {
+        //                using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+        //                {
+        //                    smtpClient.EnableSsl = true;
+        //                    smtpClient.UseDefaultCredentials = false;
+        //                    smtpClient.Credentials = loginInfo;
+        //                    smtpClient.Send(mail);
+        //                }
+
+        //            }
+        //            finally
+        //            {
+        //                //dispose the client
+        //                mail.Dispose();
+        //            }
+
+        //        }
+        //    }
+        //    catch (SmtpFailedRecipientsException ex)
+        //    {
+        //        foreach (SmtpFailedRecipientException t in ex.InnerExceptions)
+        //        {
+        //            var status = t.StatusCode;
+        //            if (status == SmtpStatusCode.MailboxBusy ||
+        //                status == SmtpStatusCode.MailboxUnavailable)
+        //            {
+        //                Response.Write("Delivery failed - retrying in 5 seconds.");
+        //                System.Threading.Thread.Sleep(5000);
+        //                //resend
+        //                //smtpClient.Send(message);
+        //            }
+        //            else
+        //            {
+        //                Response.Write("Failed to deliver message to {0}"
+        //                                  /*t.FailedRecipient*/);
+        //            }
+        //        }
+        //    }
+        //    catch (SmtpException Se)
+        //    {
+        //        // handle exception here
+        //        Response.Write(Se.ToString());
+        //    }
+
+        //    catch (Exception ex)
+        //    {
+        //        Response.Write(ex.ToString());
+        //    }
+            
+        //}
+
+        //[HttpPost]
+        //public ActionResult Contact(ContactRequest contactrequest)
+        //{
+        //    //if (ModelState.IsValid)
+        //    //{
+        //        //prepare email
+        //        var toAddress = "is403test@gmail.com";
+        //        var fromAddress = contactrequest.emailAddress.ToString();
+        //        var subject = "BlowOut Instrument Rental Inquiry: " + contactrequest.subject;
+        //        var message = new StringBuilder();
+        //        message.Append("Name: " + contactrequest.firstName + contactrequest.lastName + "\n");
+        //        message.Append("Email: " + contactrequest.emailAddress + "\n");
+        //        message.Append("phone: " + contactrequest.phoneNumber + "\n");
+        //        message.Append("Subject: " + contactrequest.subject + "\n\n");
+        //        message.Append(contactrequest.Message);
+
+        //        //start email Thread
+        //        var tEmail = new Thread(() =>
+        //       SendEmail(toAddress, fromAddress, subject, contactrequest.Message));
+        //        tEmail.Start();
+
+        //        return View("Email",contactrequest.firstName,contactrequest.lastName, contactrequest.emailAddress);
+        //    //}
+
+        //ViewBag.Name = contractrequest.firstName + " " + contractrequest.lastName;
+        //ViewBag.Email = contract.emailAddress;
+
+        //return View(contactrequest);
     }
-}
+    }
+//}
